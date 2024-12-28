0827f2bb5d1b45139c36032fb07bea19d3526a79
Hennadii <gs@telegramist.io>
2024-12-28 14:33:41 +0200

    api

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // src/config.js
 const config = {
-    api: 'http://127.0.0.1:8000/api',  // 
+    // api: 'http://127.0.0.1:8000/api',  // 
+    api: 'https://api.adve.space/'
   };
   
   export default config;
